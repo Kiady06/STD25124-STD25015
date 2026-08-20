a70daa7e20ce821b01f079a259a8f05ead2c2133
K. <148479852+Kiady06@users.noreply.github.com>
2026-08-20 09:42:47 +0300

    initial commit

---
+++ src/main/java/org/hei/binome/std25124std25015/Std25124Std25015Application.java
@@ -0,0 +1,13 @@
+package org.hei.binome.std25124std25015;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Std25124Std25015Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Std25124Std25015Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/hei/binome/std25124std25015/Std25124Std25015ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hei.binome.std25124std25015;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Std25124Std25015ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
